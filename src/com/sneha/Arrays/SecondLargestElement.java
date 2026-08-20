8575b8a1844ef733a274a325740d4fa037bcb7d0
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-08-20 13:59:11 +0530

    some add on to second largest logic

--- src/com/sneha/Arrays/SecondLargestElement.java
+++ src/com/sneha/Arrays/SecondLargestElement.java
@@ -12,20 +12,32 @@ public class SecondLargestElement {
         for(int i =0;i<n;i++){
             arr[i]=input.nextInt();
         }
+        // calculating the largest element of the array input by user.
         int largest = Integer.MIN_VALUE;
         int SMAX = Integer.MIN_VALUE;
         for(int i =0;i<n;i++){
             if(arr[i]>largest){
-                largest= arr[i];
+                largest= arr[i];  // updated largest as the max element from array
 
             }
         }
+        // Calculating second largest element of the array input by user.
         for(int i =0;i<n;i++){
-            if( arr[i]!= largest && SMAX<arr[i]){
-                SMAX = arr[i];
+            if( arr[i]!= largest && SMAX<arr[i]){     // two conditions applied the current  element should not be equal to largest and shoUld be greater than the value assigned to SMAX
+                SMAX = arr[i]; // updated SMAX as the second largest element of array input.
             }
         }
         System.out.println(  "The second largest element of the array is: " + SMAX);
         //System.out.println("Largest element is: "+largest);
     }
 }
+
+
+// MANY PEOPLE DOES THIS BY SORTING HTE ARRAY THEN PRINTING THE (N-1)th element as, if the largest element repeats  in the array  the code will fail in that scenario.
+// ex - a[]= {12, 43, -3 , 0,-2 ,-2 , 23}
+// after sorting= {-3, -2,-2,0,12,23 ,23}
+// and by the logic of printing the (n-1)th element the output will be 23
+// which is wrong
+
+
+//  you can also use else-if
