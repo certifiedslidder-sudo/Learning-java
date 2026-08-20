8232e95b7311654f589d2af52e1e4220784eddf9
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-08-20 13:39:56 +0530

    two sum -beginners approach

--- src/com/sneha/Arrays/TwoSum.java
+++ src/com/sneha/Arrays/TwoSum.java
@@ -1,1 +1,20 @@
+package com.sneha.Arrays;
+
+public class TwoSum {
+    static void main() {
+        System.out.println(" Two sum");
+    }
+
+
+    boolean twoSum(int arr[], int target) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == target)
+                    return true;
+            }
+        }
+        return false;
+    }
+}
 
