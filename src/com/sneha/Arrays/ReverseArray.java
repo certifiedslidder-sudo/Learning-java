4e33e6f4824058b225995aacf985b1b4a1479017
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-08-20 14:35:21 +0530

    reversing array elements using for loop

--- src/com/sneha/Arrays/ReverseArray.java
+++ src/com/sneha/Arrays/ReverseArray.java
@@ -3,19 +3,24 @@ package com.sneha.Arrays;
 public class ReverseArray {
     static void main()
     {
-    int[] arr = {12,34,75,9,345,34,2,5,3};
-    int n = arr.length;
-    int i = 0, j = n-1;
-    while(i<j){
+   int[] arr = {12,34,75,9,345,34,2,5,3,1};
+   int n = arr.length;
+//  for(int i = 0;i<n;i++){    // FAILSSSS as swapping will occurs for the whole array and the output will return the original array again.
+//    for(int i = 0;i<n/2;i++){   // correct condition
+//        // swap arr[i] and arr[[n-1-i]
+//        int temp = arr[i];
+//        arr[i] = arr[n-i-1];
+//        arr[n-i-1] = temp;
+//    }
+   int i = 0, j = n-1;
+   while(i<j){
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
         i++;
         j--;
     }
-        for(int val: arr)
-        {
-        System.out.print( val + " ");
-      }
+        for(int val: arr) System.out.print( val + " ");
+
     }
 }
