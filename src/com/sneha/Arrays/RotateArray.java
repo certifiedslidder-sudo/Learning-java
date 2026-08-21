449042ee5c1c61a30c1bcc1c5fe2fbb5d3833a68
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-08-21 20:30:10 +0530

    logic to rotate an array by k steps

---
+++ src/com/sneha/Arrays/RotateArray.java
@@ -0,0 +1,12 @@
+package com.sneha.Arrays;
+
+public class RotateArray {
+    public static void main(String[] args){
+        // LOGIC    for k =3
+//        FIRST REVERSE       from 0 to k-1         then reverse from           k to n - 1            now reverse the              whole array
+//                EX- arr[4,7,3,6,9,98,5]
+//        now first arr[3,7,4]                 then [5,98,9,6]                                                                   = [3,7,4,5,98,9,6]
+//        now reversing this whole all together                arr= [6,9,98,5,4,7,3]                                  this is the desired output......
+
+    }
+}
