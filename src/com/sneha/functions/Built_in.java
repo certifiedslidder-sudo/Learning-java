0971de66fc5e85d78f3c6cbe2a56d873240a2871
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-08-22 11:53:08 +0530

    math function in java

--- src/com/sneha/functions/Built_in.java
+++ src/com/sneha/functions/Built_in.java
@@ -5,12 +5,17 @@ import java.security.spec.RSAOtherPrimeInfo;
 public class Built_in {
     public static void main(String[] args){
        // double x  = Math.sqrt(100);
-      //  System.out.println(x);
-      //  System.out.println(Math.abs(-45687));   // 44687
-      //  System.out.println(Math.floor(-7.8));  // -8.0
-      // System.out.println(Math.floor(3.0001));  // 3.0
-       // System.out.println(Math.ceil(3.0001));   // 4.0
-       // System.out.println(Math.min(3.89,8));
+       // System.out.println(x);
+        System.out.println(Math.abs(-45687));   // 44687
+        System.out.println(Math.floor(-7.8));  // -8.0
+        System.out.println(Math.floor(3.0001));  // 3.0
+        System.out.println(Math.ceil(3.0001));   // 4.0
+        System.out.println(Math.min(3.89,8));
         System.out.println(Math.random());
+        System.out.println(Math.pow(-35.34, -3.33));   // error negative ki power negative integer hogi na ki negative  float
+                     // will give              NaN
+        System.out.println(Math.pow(-3.34, -2));        //0.08964107712718276
+      //  System.out.println(Math.pow(0,0);
+//
     }
 }
