4647044a469f9460ca7551dd9462d6129c3d835a
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-08-22 11:52:48 +0530

    Understanding method calling in java

---
+++ src/com/sneha/functions/Syntax.java
@@ -0,0 +1,31 @@
+package com.sneha.functions;
+public class Syntax {
+    public static void sneha(){
+        System.out.println("java");
+    }
+    static void main() {
+//        System.out.println("python");
+//        crick();
+//        sneha();         // method call
+//        System.out.println("c++");
+//        sneha();         // method call
+        suhana();
+    }
+                 //   INFINITE.........
+    static void suhana() {
+
+        System.out.println("sam");
+        aarav();
+    }
+    static void crick() {
+        System.out.println("crickk");
+        suhana();
+
+    }
+    static void aarav(){
+
+        System.out.println("no use");
+crick();
+    }
+
+}
