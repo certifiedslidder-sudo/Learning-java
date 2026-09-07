62e5b139fddba138b25016c852f659382424a6f3
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-09-07 22:35:09 +0530

    misccc

---
+++ src/com/sneha/ArrayList/AddTwoNumRepresentingTwoArray.java
@@ -0,0 +1,7 @@
+package com.sneha.ArrayList;
+    // MERGE TWO SORTED ARRAY IN A SINGLE BIG ARRAY
+public class AddTwoNumRepresentingTwoArray {
+    // example = {3,5,2,0,7} + {9,2,1} = {3,6,1,2,8}
+
+
+}
--- src/com/sneha/ArrayList/AddingOnes.java
+++ src/com/sneha/ArrayList/AddingOnes.java
@@ -2,35 +2,40 @@ package com.sneha.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 import java.util.Vector;
-public class AddingOnes {
-    static void main() {
-        class Solution {
-            // Function for adding one to the number represented by the array
-            Vector<Integer> addOne(int[] arr) {
-                Vector<Integer> ans = new Vector<>();
-                Scanner sc = new Scanner(System.in);
-                System.out.println("enter the size of array");
-                int n = sc.nextInt();
-                System.out.println("enter the array list");
-                int x = sc.nextInt();
-               // int n = arr.length;
-                int carry =1;
-                for(int i = n-1; i>=0; i--){
-                    if(arr[i] + carry <=9)
-                    {
-                        ans.add(arr[i]+carry);
-                        carry =0;
-                    }
-                    else
-                    {
-                        ans.add(0);
-                        carry =1;
-                    }
-                }
-                if(carry == 1) ans.add(1);        // for 99 or 999
-                Collections.reverse(ans);
-                return ans;
-            }
+public  class AddingOnes {
+      static class Solution {
+          // Function for adding one to the number represented by the array
+          static Vector<Integer> addOne(int[] arr) {
+              Vector<Integer> ans = new Vector<>();
+              int z = arr.length;
+              int carry = 1;
+              for (int i = z - 1; i >= 0; i--) {
+                  if (arr[i] + carry <= 9) {
+                      ans.add(arr[i] + carry);
+                      carry = 0;
+                  } else {
+                      ans.add(0);
+                      carry = 1;
+                  }
+              }
+              if (carry == 1) ans.add(1);        // for 99 or 999
+              Collections.reverse(ans);
+              return ans;
+          }
+      }
+          static void main() {
+              Scanner sc = new Scanner(System.in);
+              System.out.println("enter the size of array");
+              int n = sc.nextInt();
+              int[] arr = new int[n];
+              System.out.println("enter the array list");
+              for(int i =0; i<n;i++){
+                  arr[i] = sc.nextInt();
+              }
+              Vector<Integer> result = Solution.addOne(arr);
+
+              System.out.println("result: "+result);
+          }
         }
-    }
-}
+
+
